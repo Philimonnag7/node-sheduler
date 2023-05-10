7ed0e29446f18180998ca9c68158f69608d3371f
Philimonnag7 <philimon@appstone.in>
2023-05-10 13:32:42 +0530

    hallelujha

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT ||8000
+const schedule = require('node-schedule');
+const rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = [new schedule.Range(0, 6)];
+rule.hour = 13;
+rule.minute = 40;
+
+const job = schedule.scheduleJob(rule, function(){
+  console.log('Hallelujha God is with me...');
+});
+
+app.listen(PORT, () => { console.log(`Jesus is with me.${PORT}`) });
