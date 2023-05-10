f1dd2cebe93d3e1c955c14c9c9ccea5d2d91f4a4
Philimonnag7 <philimon@appstone.in>
2023-05-10 14:29:15 +0530

    job modified

--- index.js
+++ index.js
@@ -9,10 +9,24 @@ rule.hour = 13;
 rule.minute = 40;
 
 const job = schedule.scheduleJob(rule, function(){
-  console.log('Hallelujha God is with me...');
+  console.log('Hallelujha God is with me...rule');
 });
-const job2 = schedule.scheduleJob("**/1***", function(){
-    console.log('Hallelujha God is with me 2...');
+const job3= schedule.scheduleJob("0 */1***", function(){
+    console.log('Hallelujha God is with me...0*/1***');
   });
-
+const job4= schedule.scheduleJob("*/1***", function(){
+    console.log('Hallelujha God is with me...*/1***');
+  });
+const job5 =schedule.scheduleJob("*/23***", function(){
+    console.log('Hallelujha God is with me...*/23***');
+  });
+const job6= schedule.scheduleJob("*/24***", function(){
+    console.log('Hallelujha God is with me...*/24***');
+  });
+const job7 =schedule.scheduleJob("0*/23***", function(){
+    console.log('Hallelujha God is with me...0*/23***');
+  });
+const job8= schedule.scheduleJob("0*/24***", function(){
+    console.log('Hallelujha God is with me...0*/24***');
+  });
 app.listen(PORT, () => { console.log(`Jesus is with me.${PORT}`) });
