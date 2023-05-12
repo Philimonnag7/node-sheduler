6b9716cc01863dc0c9caee1adfe96d962fc00a88
Philimonnag7 <philimon@appstone.in>
2023-05-12 11:31:49 +0530

    node-corn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT ||8000
 const schedule = require('node-schedule');
+const cron = require('node-cron');
 const rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [new schedule.Range(0, 6)];
 rule.hour = 13;
@@ -14,19 +15,14 @@ const job = schedule.scheduleJob(rule, function(){
 const job3= schedule.scheduleJob("0 */1***", function(){
     console.log('Hallelujha God is with me...0*/1***');
   });
-const job4= schedule.scheduleJob("*/1***", function(){
-    console.log('Hallelujha God is with me...*/1***');
-  });
-const job5 =schedule.scheduleJob("*/23***", function(){
-    console.log('Hallelujha God is with me...*/23***');
-  });
-const job6= schedule.scheduleJob("*/24***", function(){
-    console.log('Hallelujha God is with me...*/24***');
-  });
-const job7 =schedule.scheduleJob("0*/23***", function(){
-    console.log('Hallelujha God is with me...0*/23***');
-  });
-const job8= schedule.scheduleJob("0*/24***", function(){
-    console.log('Hallelujha God is with me...0*/24***');
-  });
+
+cron.schedule('0 0 * * *', async () => {
+    console.log("0 0 * * *    mail sent in 24 hr")
+})
+cron.schedule('1 * * * *', async () => {
+  console.log("1 * * * *    mail sent in a 1 min")
+}) 
+cron.schedule('0 1 * * *', async () => {
+  console.log("0 1 * * *    mail sent in a 1 hr")
+})   
 app.listen(PORT, () => { console.log(`Jesus is with me.${PORT}`) });
